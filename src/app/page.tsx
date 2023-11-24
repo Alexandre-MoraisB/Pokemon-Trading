89f4ac105203f2b8524309a24952306166e7c674
Alexandre <alexandremb16@gmail.com>
2023-11-23 21:34:13 -0300

    Pokemon Trading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,221 @@
-import Image from 'next/image'
+'use client'
+import {useEffect, useState} from "react";
 
-export default function Home() {
+export default function Home(): JSX.Element {
+  const [pokemonStrings, setPokemonStrings] = useState<any[]>([])
+  const [pokemonArray, setPokemonArray] = useState<any[]>([])
+  const [userPokemons, setUserPokemons] = useState<any[]>([])
+  const [tradePokemons, setTradePokemons] = useState<any[]>([])
+
+  const [userValidName, setUserValidName] = useState<any>(true)
+  const [tradeValidName, setTradeValidName] = useState<any>(true)
+
+  const [showModal, setShowModal] = useState<any>(false)
+
+
+  useEffect(() => {
+    (async () => {
+      const tmpPokemonArray = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1300}`).then(res => res.json())
+
+      setPokemonArray(tmpPokemonArray.results)
+      setPokemonStrings(tmpPokemonArray.results.map((objeto: any) => {
+        return objeto.name
+      }))
+
+
+    })()
+  }, [])
+
+  async function addToUserInventary(mode: any = undefined): Promise<void> {
+    const input: HTMLInputElement = document.getElementById(mode === 'user' ? 'user-pokemon-input' : 'trade-pokemon-input') as HTMLInputElement
+    const nomePokemon = input.value.toLowerCase()
+    if (pokemonStrings.includes(nomePokemon)) {
+
+      // Não inlcuí exclusividade dentro do Array pois é possível se ofertar 2 "pokémons" iguais.
+      const pokemonSelecionado = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}/`)
+        .then(res => res.json())
+
+      if (mode === 'user') {
+        if (userPokemons.length >= 6) {
+          return
+        }
+        setUserPokemons(prevState => ([
+          ...prevState,
+          pokemonSelecionado
+        ]))
+      } else {
+        if (tradePokemons.length >= 6) {
+          return
+        }
+        setTradePokemons(prevState => ([
+          ...prevState,
+          pokemonSelecionado
+        ]))
+      }
+
+    } else {
+      if (mode === 'user') {
+        setUserValidName(false)
+      } else {
+        setTradeValidName(false)
+      }
+    }
+  }
+
+  function clearAlert(mode: any = undefined): void {
+    if (mode === 'user') {
+      setUserValidName(true)
+    } else {
+      setTradeValidName(true)
+    }
+  }
+
+  function openModal(): void {
+    setShowModal(!showModal)
+  }
+
+  function clearOffer(mode: any = undefined): void {
+    if (mode == 'user') {
+      setUserPokemons(() => ([]))
+    } else {
+      setTradePokemons(() => ([]))
+    }
+  }
+
+  function removePokemon(mode: any, index: number): void {
+    if (mode == 'user') {
+      const tmpArray: any[] = userPokemons
+      tmpArray.splice(index, 1)
+      setUserPokemons(() => ([
+        ...tmpArray
+      ]))
+    } else {
+      const tmpArray: any[] = tradePokemons
+      tmpArray.splice(index, 1)
+
+      setTradePokemons(() => ([
+        ...tmpArray
+      ]))
+
+    }
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className={'flex flex-grow justify-center'}>
+        <button onClick={openModal} data-modal-target="default-modal" data-modal-toggle="default-modal"
+                className="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                type="button">
+          Avaliar troca
+        </button>
+      </div>
+
+      <div className={'flex flex-row'}>
+        <div className={'m-4'}>
+          <input type='text' id='user-pokemon-input' list='pokemon-list'  onChange={() => {clearAlert('user')}}  className={`max-h-12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 ${userValidName ? 'dark:border-gray-600' : 'dark:border-red-600 '} dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}/>
+          <datalist id='pokemon-list' onClick={(event) => console.log(event.currentTarget)}>
+            {pokemonArray.map((pokemonObject) => {
+              // string contains '-' remove
+              return <option key={pokemonObject.name} value={pokemonObject.name}>{pokemonObject.name}</option>
+            })}
+          </datalist>
+
+          <button className="btn btn-blue ml-2" onClick={() => addToUserInventary('user')}>Adicionar seu pokémon</button>
+          <button className="btn btn-blue ml-2" onClick={() => clearOffer('user')}>Limpar oferta</button>
+        </div>
+
+        <div className={'flex flex-grow justify-end m-4'}>
+
+          <input type='text' id='trade-pokemon-input' list='pokemon-list'  onChange={() => {clearAlert('trade')}}  className={`max-h-12 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 ${tradeValidName ? 'dark:border-gray-600' : 'dark:border-red-600 '} dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}/>
+          <datalist id='pokemon-list' onClick={(event) => console.log(event.currentTarget)}>
+            {pokemonArray.map((pokemonObject) => {
+              return <option key={pokemonObject.name} value={pokemonObject.name}>{pokemonObject.name}</option>
+            })}
+          </datalist>
+
+          <button className="btn btn-blue ml-2"onClick={() => addToUserInventary('trade')}>Adicionar pokémon para trocar</button>
+          <button className="btn btn-blue ml-2" onClick={() => clearOffer('trade')}>Limpar oferta</button>
         </div>
       </div>
+      <div className={'grid grid-cols-2 gap-x-20'}>
+        <div className={'grid grid-cols-3 gap-4'}>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+          {userPokemons.map((pokemonObject, index: number) => {
+            return <div key={pokemonObject.id} className="col-2 card bg-base-100 shadow-xl min-h-[20em] max-h-[20em]">
+              <figure><img src={pokemonObject.sprites.front_default} alt={pokemonObject.name} /></figure>
+              <div className="card-body">
+                <h2 className="card-title">{pokemonObject.name}</h2>
+                <p>Valor de experiência base: {pokemonObject.base_experience}</p>
+                <div className="card-actions justify-end">
+                  <button onClick={() => {removePokemon('user', index)}} className="btn btn-danger">Remover</button>
+                </div>
+              </div>
+            </div>
+          })}
+
+        </div>
+
+        <div className={'grid grid-cols-3 gap-4'}>
+
+          {tradePokemons.map((pokemonObject, index: number) => {
+            return <div key={pokemonObject.id} className="col-2 card bg-base-100 shadow-xl min-h-[20em] max-h-[20em]">
+              <figure><img src={pokemonObject.sprites.front_default} alt={pokemonObject.name} /></figure>
+              <div className="card-body">
+                <h2 className="card-title">{pokemonObject.name}</h2>
+                <p>Valor de experiência base: {pokemonObject.base_experience}</p>
+                <div className="card-actions justify-end">
+                  <button onClick={() => {removePokemon('trade', index)}} className="btn btn-danger">Remover</button>
+                </div>
+              </div>
+            </div>
+          })}
+
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {showModal &&
+        <div id="default-modal" tabindex="-1" aria-hidden="true" className="overflow-y-auto overflow-x-hidden ml-[33%] mt-[10%] fixed z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  Avaliação de troca
+                </h3>
+                <button onClick={() => {setShowModal(false)}} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                  <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                  </svg>
+                </button>
+              </div>
+              <div className="p-4 md:p-5 space-y-4">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    O limite de pokémons que podem ser ofertados, para cada lado, é 6.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  O valor experiência de seus pokémons em total é de: {userPokemons.reduce((a,v) =>  a = a + v.base_experience , 0 )}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  O valor experiência dos pokémons a serem rebidos é de: {tradePokemons.reduce((a,v) =>  a = a + v.base_experience , 0 )}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    A diferença de pontos entre os pokémons é de: {Math.abs(tradePokemons.reduce((a,v) =>  a = a + v.base_experience , 0 ) - userPokemons.reduce((a,v) =>  a = a + v.base_experience , 0 ))}
+                </p>
+                {Math.abs(tradePokemons.reduce((a,v) =>  a = a + v.base_experience , 0 ) - userPokemons.reduce((a,v) =>  a = a + v.base_experience , 0 )) >= 50 ?
+                  <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      A troca ofertada é injusta.
+                  </p>
+                : <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                      A troca ofertada pode ser justa, pois diferença entre pontos é pequena. Avalie suas necessidades e o quão bem os pokémons se adaptariam a sua composição.
+                  </p>
+                }
+              </div>
+              <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button onClick={() => {setShowModal(false)}} data-modal-hide="default-modal" type="button" className="ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Ok</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+    </>
   )
 }
